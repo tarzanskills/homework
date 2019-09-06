f951c055484fc7038421ae0c8cb3965f05fcee85
sachinsuresh <sachinsuresh00@gmail.com>
2019-09-06 21:26:54 +0530

    Does string operations on the given input, my full name

---
+++ string_operations.py
@@ -0,0 +1,13 @@
+# input = "Sachin Suresh Kumar"
+
+string_name = "Sachin Suresh Kumar"
+
+print(string_name[0:6])     #first name
+print(string_name[7:13])    #father's anme
+print(string_name[14:19])   #surname
+print(string_name[::-1])    #name in reverse order
+print(string_name[::2])     #alt char in name
+print(string_name[-2::-2])  #every sec char in rev
+print(string_name[5:-20:-1])#first name reverse
+print(string_name[12:6:-1]) #father's name reverse
+
