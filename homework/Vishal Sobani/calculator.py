acabb9853ee744c89c146c68f46524e77076b378
vishal sobani <vishal.sobani@gmail.com>
2019-09-07 15:16:03 +0530

    homework for day 3 by Vishal

---
+++ homework/Vishal Sobani/calculator.py
@@ -0,0 +1,39 @@
+def addition(num1,num2):
+    sum=num1+num2
+    return sum
+
+def subtract(num1,num2):
+    sub=num1-num2
+    return sub
+
+def multiply(num1,num2):
+    mul=num1*num2
+    return mul
+
+
+def divide(num1,num2):
+    div=num1/num2
+    return div
+
+num1=int(input("enter the value of num1: "))
+num2=int(input("enter the value of num2: "))
+s
+operation = input("Select operation that you want to perform: ")
+
+if operation == "add":
+    print(addition(num1,num2))
+elif operation == "subtract":
+    print(subtract(num1,num2))
+elif operation == "multiply":
+    print(multiply(num1, num2))
+elif operation == "divide":
+    print(divide(num1,num2))
+else:
+    print("Please select a valid operation like add, subtract, divide etc.")
+
+# print(addition(a,b))
+# print(subtract(a,b))
+#
+# print(multiply(a,b))
+#
+# print(divide(a,b))
