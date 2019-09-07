5c37e8581c4184654e8a69287e1a2864d1d01592
Venkatesh Kumar <venkatesh.vk677@gmail.com>
2019-09-07 15:48:52 +0530

    homework done

---
+++ venkatesh/day-3-venkatesh.py
@@ -0,0 +1,7 @@
+first_name=input("enter your name")
+fathers_name=input("enter your fathers_name")
+print(first_name)
+print(first_name[::-1])
+print(first_name[0::2])
+print(first_name[::-2])
+print(fathers_name[::-1])
---
+++ venkatesh/day33-hw-3.py
@@ -0,0 +1,5 @@
+name=input("enter your name")
+
+def reverse(name):
+    return name[::-1]
+print(reverse(name))
