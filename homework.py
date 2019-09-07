f753094b3eb2be90a733c02c704e4463dc74e383
saurabhnayak <saurabhnk94@gmail.com>
2019-09-07 08:31:51 +0530

    HOME work on name

---
+++ homework.py
@@ -0,0 +1,12 @@
+#Home Work
+my_full_name = "SAURABH NAYAK"
+print(my_full_name[:7])  #SAURABH
+print(my_full_name[8:])  #NAYAK
+print(my_full_name[::-1]) #REVERSE ORDER
+print(my_full_name[::2]) #Alternate character
+print(my_full_name[::-3])  #Alternate 2nd character in reverse
+print(my_full_name[6::-1])  #first name in reverse order
+
+
+
+
