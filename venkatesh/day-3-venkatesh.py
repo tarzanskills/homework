127a2bf13a3457389bd289c55ef4e02900cc927a
Venkatesh Kumar <venkatesh.vk677@gmail.com>
2019-09-07 12:30:45 +0530

    day-3 homework

---
+++ venkatesh/day-3-venkatesh.py
@@ -0,0 +1,8 @@
+first_name=input("enter your first_name")
+print(first_name)
+fathers_name=input("enter your fathers_name")
+print(fathers_name)
+surname=input("enter your surname")
+print(surname)
+print(first_name[::-1])
+[::2]
