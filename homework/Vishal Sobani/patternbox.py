3d42e6af8c108b543f88418f3b03ce63caa190ff
vsobani <vishal.sobani@gmail.com>
2019-09-10 00:24:49 +0530

    adding pattern program

--- homework/Vishal Sobani/patternbox.py
+++ homework/Vishal Sobani/patternbox.py
@@ -1,23 +1,19 @@
-def minimum_value(row_index,col_index):
-    if row_index<=col_index:
-        return row_index
-    else:
-        return col_index
+
+dimension  =  int(input("Enter the dimension : "))
+fact_1  =  2*dimension-1
+fact_2  =  2*dimension-2
 
 
-dimension=int(input("Enter the dimension : "))
-fact_1=2*dimension-1
-fact_2=2*dimension-2
-for row in range(fact_1):
+for row in range (fact_1):
     print()
-    for col in range(fact_1):
-        if row>=dimension:
-            if col>=dimension:
-                print(dimension-min((fact_2)-row,(fact_2)-col),end='')
+    for col in range (fact_1):
+        if row  >=  dimension:
+            if col  >=  dimension:
+                print (dimension - min( (fact_2) - row ,(fact_2) - col) , end='')
             else:
-                print(dimension-min((fact_2)-row,col),end='')
+                print (dimension - min( (fact_2) - row , col ) , end='')
         else:
-            if(col>=dimension):
-                print(dimension-min(row,(fact_2)-col),end='')
+            if(col  >=  dimension):
+                print (dimension - min(row,(fact_2) - col ), end='')
             else:
-                print(dimension-min(row,col),end='')
+                print( dimension - min (row,col) , end='')
