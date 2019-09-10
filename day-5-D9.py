7804e4eb72a61a03edc124632b98207d9c17504d
SaneeyaIqbal <s.saneeya.iqbal@gmail.com>
2019-09-10 21:01:02 +0530

    difference of sum of squares

---
+++ day-5-D9.py
@@ -0,0 +1,18 @@
+sum = 0
+num = 0
+while num <= 100:
+    sum = sum + num
+    num = num + 1
+sum_of_squares = sum**2
+print(sum_of_squares)
+
+num1 = 0
+sum1 = 0
+
+while num1 <= 100:
+    number_of_square =num1**2
+    sum1=sum1+number_of_square
+    num1 = num1+1
+print(sum1)
+difference = sum_of_squares-sum1
+print(difference)
